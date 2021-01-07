8fa2b0541e2321f8e5f578c0c21cedef870f78f0
hbj <heyhal19@gmail.com>
2021-01-06 17:04:34 -0800

    missing changes

--- src/App.js
+++ src/App.js
@@ -9,73 +9,12 @@ class App extends React.Component {
     started: false,
     redHand: [],
     blueHand: [],
-    cards: {
-        "♥": [
-          2,
-          3,
-          4,
-          5,
-          6,
-          7,
-          8,
-          9,
-          10,
-          11,
-          12,
-          13,
-          14
-        ],
-        "♠": [
-          2,
-          3,
-          4,
-          5,
-          6,
-          7,
-          8,
-          9,
-          10,
-          11,
-          12,
-          13,
-          14
-        ],
-        "♦": [
-          2,
-          3,
-          4,
-          5,
-          6,
-          7,
-          8,
-          9,
-          10,
-          11,
-          12,
-          13,
-          14
-        ],
-        "♣": [
-          2,
-          3,
-          4,
-          5,
-          6,
-          7,
-          8,
-          9,
-          10,
-          11,
-          12,
-          13,
-          14
-        ]
-      }
+    suits: ["♥", "♠", "♦", "♣"]
   }
 
   startGame = () => {
-    this.setState({started: true})
-    this.initialDeal()
+    this.setState({ started: true })
+    this.shuflr()
   }
 
   initialDeal = () => {
@@ -85,32 +24,40 @@ class App extends React.Component {
     this.shuflr(this.state.cards["♠"], "♠")
   }
 
-  shuflr = (suit, name) => {
-    let tempBlue = this.state.blueHand;
-    let tempRed = this.state.redHand;
-
-    suit.forEach(card => {
-      if(tempBlue.length>= 26){
-        tempRed.push(`${card}${name}`)
+  shuflr = () => {
+    let blueHand = [];
+    let redHand = [];
+    let fullShuffle = []
+    let counter = 0
+    this.state.suits.forEach(suit => {
+      for (let i = 2; i < 15; i++) {
+        console.log(++counter)
+        let rand = Math.floor(Math.random() * 2)
+        if (rand && blueHand.length < 26) {
+          // add to blueHand
+          blueHand.push(`${i} of ${suit}`)
+        }
+        else if (redHand.length < 26) {
+          // add to redHand
+          redHand.push(`${i} of ${suit}`)
+        }
+        else {
+          // winds up with a chunk of leftovers being sent to the blue hand in order -- no good
+          blueHand.push(`${i} of ${suit}`)
+        }
       }
-      else if(tempRed.length>=26){
-        tempBlue.push(`${card}${name}`)
-      }
-      else if((Math.floor(Math.random()*100))%2 === 0){
-        tempBlue.push(`${card}${name}`)
-      }
-      else if(tempRed) tempRed.push(`${card}${name}`)
     })
-    this.setState({redHand: tempRed, blueHand: tempBlue})
+    console.log(redHand, blueHand)
+    // this.setState({redHand, blueHand})
   }
 
   render() {
-      return (
+    return (
       <div className="App">
-        {this.state.started?
-        <Gamepage redHand={this.state.redHand} blueHand={this.state.blueHand}/>
-        :
-        <Homepage startGame={this.startGame}/>
+        {this.state.started ?
+          <Gamepage redHand={this.state.redHand} blueHand={this.state.blueHand} />
+          :
+          <Homepage startGame={this.startGame} />
         }
       </div>
     );
